4afb19a03fd38cdd2a6120e5d4376a6d7e469bc4
Md Emon Hossen <mdemong87@gmail.com>
2025-10-15 16:42:56 +0600

    update

--- js/payment/payment.js
+++ js/payment/payment.js
@@ -412,7 +412,7 @@ class PaymentPage {
     }
 
     continueShopping() {
-        window.location.href = 'products.html';
+        window.location.href = 'product.html';
     }
 
     viewOrderDetails() {
